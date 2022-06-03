2828e5ec143da9e25aa520a19417ea4fa184920d
MRZ <96188798+BD-MRZ-KING@users.noreply.github.com>
2022-06-03 15:32:56 +0600

    Add files via upload

---
+++ x2.py
@@ -0,0 +1,13 @@
+import time
+
+print("\n\x1b[1;92m Congratulations ! Your Device Support This Tool")
+
+print("\n\x1b[1;97m Hi ! Looks like you are a new user. Thanku you for choosing Younis John as your trusted choice. Don't forget to give the best rating on Github yes! Thank you!  :^))")
+
+print("\n\x1b[1;91m\x1b[1;43m Disclaimer! \x1b[0m \033[1;92mThis Tool Is Coded By Younis John It Is Only For Educational Perpose Any Illegal Use You Face It Yourself Thanku :) \033[1;37m");time.sleep(7)
+
+print("")
+
+import X2
+
+X2.menu()
